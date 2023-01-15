c033fb4253463888140333bbe5100820c064a99f
Coyote21 <hizuko@me.com>
2023-01-15 13:36:39 +1100

    Add project files.

---
+++ FoodRemoverPlus/Program.cs
@@ -0,0 +1,22 @@
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Synthesis;
+using Mutagen.Bethesda.Skyrim;
+
+namespace FoodRemoverPlus
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            return await SynthesisPipeline.Instance
+                .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
+                .SetTypicalOpen(GameRelease.SkyrimSE, "YourPatcher.esp")
+                .Run(args);
+        }
+
+        public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+            //Your code here!
+        }
+    }
+}
